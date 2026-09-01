427ddc46d5f07be6ed70c07d75f9eee3b8f502b0
Ardan <ramadhanardan69@gmail.com>
2026-09-01 10:50:29 +0700

    feat: implement JWT authentication API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 // use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -8,6 +9,20 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
+Route::prefix('auth')->name('auth.')->group(function () {
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+
+    Route::middleware('jwt')->group(function () {
+        Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
+});
+
+Route::middleware('jwt')->group(function () {
+    Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+});
 
 // Product
 Route::get('/products', [ProductController::class, 'index'])
